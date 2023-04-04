a107cb85f1d55a2b0f07b089b40af5ab48427219
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-04 01:18:39 -0400

    Adding the head function and removing the trailing spaces

--- head.js
+++ head.js
@@ -12,7 +12,7 @@ const head = function(arrayElements) {
   return firstElement;
 };
 
-// TEST CODE 
+// TEST CODE
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hi");
