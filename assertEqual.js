266db3e38057caeeff274640382f50de63c48d61
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-04 00:43:03 -0400

    assertEqual added

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🥳🎉🥳 " + "Assertion Passed: " + [actual] + " === " + [expected]);
+  } else {
+    console.log("😰❌😰 " + "Assertion Failed: " + [actual] + " !== " + [expected]);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual("Hello", "hello");
+assertEqual(24, 24);
+assertEqual(24, 99);
