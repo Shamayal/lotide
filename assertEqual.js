69a153d3b04bb56545ef6b5b0612d3aa7ebe3629
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-04 00:55:22 -0400

    assertEqual function updated with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🥳🎉🥳 " + "Assertion Passed: " + [actual] + " === " + [expected]);
+    console.log(`🥳🎉🥳 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("😰❌😰 " + "Assertion Failed: " + [actual] + " !== " + [expected]);
+    console.log(`😰❌😰 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
