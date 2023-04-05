11a3fd9a1d7dc402934e552f74a3fddf8c024413
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-05 08:09:43 -0400

    Function to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🥳🎉🥳 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😰❌😰 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  let output = true;
+  if (firstArray.length === secondArray.length) {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] === secondArray[i]) {
+        output = output && true;
+      } else {
+        output = output && false;
+      }
+    }
+  } return output;
+};
+// TEST CODE
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 5, 3]), false); // => should FAIL
