68fa65a4f8c2e908ace0cd3a8778c25fef9d7231
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-06 22:58:49 -0400

    Function to find keys by their value

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🥳🎉🥳 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😰❌😰 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(objectToSearch, valueToFind) {
+  for (let objKey in objectToSearch) {
+    if (objectToSearch[objKey] === valueToFind) {
+      return objKey;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const bestDessertByMonth = {
+  December: "gingerbread cookies",
+  July: "ice-cream",
+  May: "lemon tart",
+  August: "ice-cream",
+  October: "pumpkin pie"
+};
+
+assertEqual(findKeyByValue(bestDessertByMonth, "pumpkin pie"), "October");
+assertEqual(findKeyByValue(bestDessertByMonth, "cinnamon rolls"), undefined);
+assertEqual(findKeyByValue(bestDessertByMonth, "lemon tart"), "May");
+assertEqual(findKeyByValue(bestDessertByMonth, "ice-cream"), "July");
