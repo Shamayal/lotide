5a1f4f3000804fee9e0f1bbb6fb59efefb1f23b6
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-11 09:36:35 -0400

    flatten function

---
+++ flatten.js
@@ -0,0 +1,43 @@
+const assertArraysEqual = function(firstArray, secondArray) {
+  let actual = eqArrays(firstArray, secondArray);
+  if (actual === true) {
+    console.log(`🥳🎉🥳 Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    console.log(`😢❌😢 Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  let output = true;
+  if (firstArray.length === secondArray.length) {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] === secondArray[i]) {
+        output = output && true;
+      } else {
+        output = output && false;
+      }
+    }
+  } else {
+    output = false;
+  }
+  return output;
+};
+
+const flatten = function(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i]) === true) {
+      for (let j = 0; j < array[i].length; j++) {
+        newArray.push(array[i][j]);
+      }
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([[18], 94, [71, 36], [67, 99]]), [18, 94, 71, 36, 67, 99]);
+assertArraysEqual(flatten([]), []);
+assertArraysEqual(flatten([100]), [100]);
